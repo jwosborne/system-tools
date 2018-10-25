ceccc2b340e03d4feb7a2a4f45ecb841274589aa
john <john@debian.eva>
2018-10-25 16:42:41 -0400

    added echo

---
+++ echo.c
@@ -0,0 +1,18 @@
+#include "echo.h"
+
+/* echo -- echo  command line arguments to output */
+void echo(int argc, char const *argv[]) {
+    if (argc <= 1) {
+        printf("\n");
+    }
+    else {
+        for (int i = 1; i < argc; i++) {
+            printf("%s", argv[i]);
+
+            if(i < argc - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}

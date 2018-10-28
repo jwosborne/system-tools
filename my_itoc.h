c058e5c6b6109c1dc963569b7523cc8971390939
john <john@debian.eva>
2018-10-28 11:07:50 -0400

    fixed my_itoc.h include gaurds

--- my_itoc.h
+++ my_itoc.h
@@ -1,4 +1,13 @@
+#ifndef STDIO_H
+#define STDIO_H
 #include <stdio.h>
+#endif
+
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
 #include <stdlib.h>
 
 void my_itoc(int n, char* str, int i);

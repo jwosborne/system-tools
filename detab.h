26e168c72dd95cd106f5efccdad2940fc9f5dbc4
john <john@debian.eva>
2018-11-01 14:28:08 -0400

    more style fixes pt2

--- detab.h
+++ detab.h
@@ -1,18 +1,11 @@
 #include "settabs.h"
 #include "tabpos.h"
 
-#ifndef STDBOOL_H
-#define STDBOOL_H
-#include <stdbool.h>
-#endif
-
 #ifndef STDIO_H
 #define STDIO_H
 #include <stdio.h>
 #endif
 
-#ifndef MAXLINE
-#define MAXLINE 1000
-#endif
+/* MAXLINE defined in putrep.h */
 
 void detab(void);
